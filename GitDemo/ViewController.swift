cf022990b6d5a9d965bfe45b46eb24a343a51ade
Pavel <poulkravtsoff@mail.ru>
2018-09-10 17:51:23 +0300

    Add new print statement in VC swift

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Another print statement")
     }
 
     override func didReceiveMemoryWarning() {
