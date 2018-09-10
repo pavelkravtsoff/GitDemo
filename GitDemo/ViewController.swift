cd37fae370e772da3c76653bb9ded8ec24ee0dcc
Pavel <poulkravtsoff@mail.ru>
2018-09-10 13:40:14 +0300

    initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by JigSaw on 10.09.2018.
+//  Copyright © 2018 poulkravtsoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
